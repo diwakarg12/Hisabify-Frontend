9ba43be0115f9cde64d4a1db5f154b8f65e51e0d
Harsh Raj <rkyharsu@gmail.com>
2025-05-14 23:51:25 +0530

    Header Component Completed with dynamic current date . using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Header from "./Component/Common/Header/Header";
 import SideNav from "./Component/Common/SideNavBar/SideNav";
 
 
 function App() {
 
   return (
-    <div className="flex flex-row bg-gray-300" >
+    <div className="flex flex-col bg-gray-300" >
+      <Header />
       <SideNav />
     </div>
   )
