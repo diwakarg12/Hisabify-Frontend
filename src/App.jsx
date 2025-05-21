97525e8efd309edc659e7a324bc6185dc4ce4537
Harsh Raj <rkyharsu@gmail.com>
2025-05-21 15:40:18 +0530

    Ui of login and SignUp complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./Component/Common/Header/Header";
 import SideNav from "./Component/Common/SideNavBar/SideNav";
+import Loginsignup from "./Component/Pages/Login_Signup/loginsignup";
 
 
 function App() {
 
   return (
     <div className="flex flex-col bg-gray-300" >
-      <Header />
-      <SideNav />
+      <Loginsignup />
+      {/* <Header />
+      <SideNav /> */}
+     
     </div>
   )
 }
