e38db0cc7dd834a8e40e2ad77b07927ec57fce31
Harsh Raj <rkyharsu@gmail.com>
2025-05-21 16:27:24 +0530

    Routes Configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Header from "./Component/Common/Header/Header";
 import SideNav from "./Component/Common/SideNavBar/SideNav";
 import Loginsignup from "./Component/Pages/Login_Signup/loginsignup";
+import RoutesConfig from "./config/RoutesConfig";
 
 
 function App() {
 
   return (
     <div className="flex flex-col bg-gray-300" >
-      <Loginsignup />
       {/* <Header />
       <SideNav /> */}
-     
+      <RoutesConfig />
     </div>
   )
 }
