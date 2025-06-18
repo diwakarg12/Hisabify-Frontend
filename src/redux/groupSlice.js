246854e9c9d74423b9b325621941dfd0636eb929
Diwakar Giri <diwakargiri23@gmail.com>
2025-06-18 16:15:34 +0530

    fix: modify auth and group slice to include redux thunk middleware to call all the apis inside redux only to manage the apis and redux correctly and easily. fix: add and configure react toastify for notification purpose

--- src/redux/groupSlice.js
+++ src/redux/groupSlice.js
@@ -1,36 +1,190 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
     groups: [],
     isAddGroupFormOpen: false,
+    loading: false,
+    error: null
 }
+
+export const createGroup = createAsyncThunk('createGroup', async (data, { rejectWithValue }) => {
+    try {
+
+        const response = await fetch('/api', {
+            method: 'POST',
+            headers: {
+                "Content-type": "Application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        return result;
+
+    } catch (error) {
+        rejectWithValue(error);
+    }
+});
+
+export const updateGroup = createAsyncThunk('updateGroup', async ({ data, groupId }, { rejectWithValue }) => {
+    try {
+
+        const response = await fetch(`/api/${groupId}`, {
+            method: 'PORT',
+            headers: {
+                "Content-type": "Application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        return result;
+
+    } catch (error) {
+        rejectWithValue(error)
+    }
+});
+
+export const removeUser = createAsyncThunk('removeUser', async ({ groupId, userId }, { rejectWithValue }) => {
+    try {
+
+        const response = await fetch(`/api/${groupId}/${userId}`, {
+            method: 'POST',
+            headers: {
+                "Content-type": "Application/json"
+            },
+        });
+
+        const result = await response.json();
+        return result;
+
+    } catch (error) {
+        rejectWithValue(error)
+    }
+});
+
+export const deleteGroup = createAsyncThunk('deleteGroup', async (groupId, { rejectWithValue }) => {
+    try {
+
+        const response = await fetch(`/api/${groupId}`, {
+            method: 'POST',
+            headers: {
+                "Content-type": "Application/json"
+            },
+        });
+        const result = await response.json();
+        return result;
+
+    } catch (error) {
+        rejectWithValue(error)
+    }
+});
+
+export const getAllGroup = createAsyncThunk('getAllGroup', async (_, { rejectWithValue }) => {
+    try {
+
+        const response = await fetch(`/api`, {
+            method: 'GET',
+            headers: {
+                "Content-type": "Application/json",
+            },
+        });
+
+        const result = await response.json();
+        return result;
+
+    } catch (error) {
+        rejectWithValue(error);
+    }
+})
 
 const groupSlice = createSlice({
     name: 'Group',
     initialState,
     reducers: {
-        setGroups: (state, action) => {
-            state.groups = action.payload
-        },
-        addGroup: (state, action) => {
-            state.groups = [...state.groups, action.payload];
-        },
-        removeGroup: (state, action) => {
-            state.groups = state.groups.filter(group => group._id !== action.payload)
-        },
-        updateGroup: (state, action) => {
-            state.groups = state.groups.map(group => (
-                group._id.toString() === action.payload._id.toString() ? (
-                    { ...group, groupName: action.payload.groupName }
-                ) : group
-            ))
-        },
         toggleAddGroupForm: (state) => {
             state.isAddGroupFormOpen = !state.isAddGroupFormOpen
         }
-    }
+    },
+    extraReducers: (builder) => {
+        builder
+            //getAllGroup
+            .addCase(getAllGroup.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(getAllGroup.fulfilled, (state, action) => {
+                state.loading = false;
+                state.groups = action.payload;
+                state.error = null;
+            })
+            .addCase(getAllGroup.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
 
+            //createGroup
+            .addCase(createGroup.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(createGroup.fulfilled, (state, action) => {
+                state.loading = true;
+                state.groups.push(action.payload);
+                state.error = null;
+            })
+            .addCase(createGroup.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+            //updateGroup
+            .addCase(updateGroup.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(updateGroup.fulfilled, (state, action) => {
+                state.loading = false;
+                state.groups = state.groups.map(group => group._id.toString() === action.payload._id.toString() ? action.payload : group);
+                state.error = null;
+            })
+            .addCase(updateGroup.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+            //removeUserFromGroup
+            .addCase(removeUser.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(removeUser.fulfilled, (state, action) => {
+                state.pending = false;
+                state.groups = state.groups.map(group => {
+                    if (group.members.some(user => user._id === action.payload)) {
+                        return {
+                            ...group,
+                            members: group.members.filter(user => user._id !== action.payload)
+                        };
+                    };
+                });
+                state.error = null;
+            })
+            .addCase(removeUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+
+            //removeGroup
+            .addCase(deleteGroup.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(deleteGroup.fulfilled, (state, action) => {
+                state.loading = false;
+                state.groups = state.groups.filter(group => group._id.toString() !== action.payload.toString());
+                state.error = null;
+            })
+            .addCase(deleteGroup.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
+    }
 });
 
-export const { addGroup, removeGroup, updateGroup, toggleAddGroupForm } = groupSlice.actions;
+export const { toggleAddGroupForm } = groupSlice.actions;
 export default groupSlice.reducer;
