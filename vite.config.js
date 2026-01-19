292106a43b551fdf757caac0ddcd23e92541fe03
Diwakar Giri <diwakargiri23@gmail.com>
2026-01-19 17:30:58 +0530

    fixed a basic bug in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 /* eslint-disable no-undef */
 /* eslint-env node */
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode`
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig({
 
-  return {
-    plugins: [react(), tailwindcss()],
-    base: env.VITE_BASE_PATH || "/",
-  };
+  plugins: [react(), tailwindcss()],
+  base: "/",
 });
