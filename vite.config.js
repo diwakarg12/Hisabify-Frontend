b19223315cf82b3fbd5a1838684903029fd1952f
Diwakar Giri <diwakargiri23@gmail.com>
2026-01-19 17:44:01 +0530

    fix vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-undef */
-/* eslint-env node */
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,5 +6,4 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
 
   plugins: [react(), tailwindcss()],
-  base: "/",
 });
